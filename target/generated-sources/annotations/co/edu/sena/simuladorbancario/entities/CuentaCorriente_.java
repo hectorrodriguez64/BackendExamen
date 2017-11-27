4c4ec86ac52494cc5470a3dd4ab63b3040a83db1
Adsi 1261718 <hefaromu.jc0526@gmail.com>
2017-11-27 10:34:15 -0500

    Cambios

--- target/generated-sources/annotations/co/edu/sena/simuladorbancario/entities/CuentaCorriente_.java
+++ target/generated-sources/annotations/co/edu/sena/simuladorbancario/entities/CuentaCorriente_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-27T10:21:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-27T10:33:09")
 @StaticMetamodel(CuentaCorriente.class)
 public class CuentaCorriente_ { 
 
